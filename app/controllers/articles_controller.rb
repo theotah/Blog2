8aac020f1fda09336bfa224ed31a502d012ad167
theotah <theotah79@gmail.com>
2019-04-05 11:15:26 +0000

    styled1 blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ def create
 
     if @article.save
 
-    flash[:notice] = "Article was successfully created"
+    flash[:success] = "Article was successfully created"
 
     redirect_to article_path(@article)
 
@@ -46,7 +46,7 @@ def update
 
     if @article.update(article_params)
 
-    flash[:notice] = "Article was successfully updated"
+    flash[:success] = "Article was successfully updated"
 
     redirect_to article_path(@article)
 
@@ -73,7 +73,7 @@ def destroy
 
     @article.destroy
 
-    flash[:notice] = "Article was successfully deleted"
+    flash[:success] = "Article was successfully deleted"
 
     redirect_to articles_path
 
