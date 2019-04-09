f91b6c8a29977721b8749211fbbcb8b8b5e9107c
theotah <theotah79@gmail.com>
2019-04-09 08:43:50 +0000

    added user show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -84,9 +84,9 @@ private
 
 def set_article
         @article = Article.find(params[:id])
-end    
+end
+
 def article_params
 params.require(:article).permit(:title, :description)
-
 end  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+def index
+    @users = User.all
+end
+
 def new
     @user = User.new
 end
@@ -14,6 +18,26 @@ def create
     render 'new'
     end
 end
+
+def edit
+
+    @user = User.find(params[:id])
+
+end
+
+    def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    flash[:success] = "Your account was updated successfully"
+    redirect_to articles_path
+    else
+    render 'edit'
+    end
+    end
+    
+def show
+    @user = User.find(params[:id])
+end    
 
 private
 
